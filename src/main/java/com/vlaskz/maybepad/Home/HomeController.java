32d05d32fc749fbd90c6f2a7d5c296c21b960ef7
Isaias Velasquez <ivelasquez-rl@rocketlawyer.com.br>
2023-08-08 00:37:12 -0300

    feat: add dummy home

--- src/main/java/com/vlaskz/maybepad/Home/HomeController.java
+++ src/main/java/com/vlaskz/maybepad/Home/HomeController.java
@@ -1,13 +1,26 @@
 package com.vlaskz.maybepad.Home;
 
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class HomeController {
-    @RequestMapping("/{path:[^.]*}")
-    public String forward(@PathVariable String path){
-        return "forward:/";
+
+    @GetMapping("/")
+    @ResponseBody
+    public Resource home() {
+        return new ClassPathResource("static/home.html");
+    }
+
+    @GetMapping("/{path:[^.]*}")
+    @ResponseBody
+    public Resource dynamicRouteHandler(@PathVariable String path) {
+        return new ClassPathResource("static/defaultPage.html");
     }
 }
--- src/main/java/com/vlaskz/maybepad/Page/PageController.java
+++ src/main/java/com/vlaskz/maybepad/Page/PageController.java
@@ -1,5 +1,6 @@
 package com.vlaskz.maybepad.Page;
 
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,8 +9,10 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api")
 public class PageController {
+
     @Value("${page.helpId}")
     private String helpId;
+
     @Value("${page.homeId}")
     private String homeId;
 
@@ -19,22 +22,29 @@ public class PageController {
         this.pageRepository = pageRepository;
     }
 
+    // Acessar a página inicial
     @GetMapping("/")
     public String getHomePage(){
-        return "home";
+        return "home";  // Isto retorna a visualização 'home' (ou home.html)
     }
+
+    // Obter uma página baseada no path
     @GetMapping("/{path}/**")
     public ResponseEntity<Page> getPage(@PathVariable String path) {
         Page page = pageRepository.findByPath("/" + path);
-        if(page == null) {
+
+        if (page == null) {
             page = pageRepository.findByPath(helpId);
+
             if (page == null) {
                 return new ResponseEntity<>(HttpStatus.NOT_FOUND);
             }
         }
+
         return new ResponseEntity<>(page, HttpStatus.OK);
     }
 
+    // Salvar uma página
     @PostMapping("/{path}/**")
     public ResponseEntity<Page> savePage(@PathVariable String path, @RequestBody Page page) {
         pageRepository.save(page);
